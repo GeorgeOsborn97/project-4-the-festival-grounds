d8a34430820e729d50c26d07fe6058e6dbd6479e
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-01-19 14:12:53 +0000

    link up first view to index template

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Rooms
 
 # Create your views here.
+
+
+class RoomList(generic.ListView):
+    model = Rooms
+    queryset = Rooms.objects.order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 5
