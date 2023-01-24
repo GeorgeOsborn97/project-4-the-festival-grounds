a66d17cc4fdb0768742c43d3e400c8d8d1690183
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-01-24 11:00:19 +0000

    move pagination form base.html to index.html and begin to implement search functionss

---
+++ static/javascript/script.js
@@ -0,0 +1,16 @@
+/*$('#room-search').click(function(event){
+    $('#card-row > div').each(function () {
+         if( $(this).attr("id").indexOf(document.getElementById("room-search-input").value) == -1){
+            console.log($(this).attr("id"))
+            $(this).hide()
+         }
+    })
+    console.log(document.getElementById("room-search-input").value)
+})*/
+
+$("#room-search").click(function() {
+    var value = $('#room-search-input').val().toLowerCase();
+    $('div[data-role="test"]').filter(function() {
+        $(this).toggle($(this).find('h5').text().toLowerCase().indexOf(value) > -1)
+    });
+});
