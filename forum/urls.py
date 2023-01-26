66d4d0926ff869d6f60b5c16e86b3a4626656bc6
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-01-26 14:18:47 +0000

    add a room view that allows the user to see conversations

--- forum/urls.py
+++ forum/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.RoomList.as_view(), name='home'),
     path('add_room', views.addRoom.as_view(), name='add'),
+    path('<slug:slug>/', views.RoomView.as_view(), name='in_room'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -3,48 +3,17 @@ from django.core.paginator import Paginator
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from .models import Rooms
+from .models import Rooms, Conversations, Comments
 from .forms import RoomForm
 
 # Create your views here.
 
 
-#class RoomList(generic.ListView):
-#    model = Rooms
-#    queryset = Rooms.objects.order_by('created_on')
-#    template_name = 'index.html'
-#    paginate_by = 5
-
-
-class RoomList(View):
-
-    def get(self, request, *args, **kwargs):
-        room = Rooms.objects.order_by('created_on')  
-        paginator = Paginator(room, 5)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        return render(
-            request,
-            'index.html',
-            {
-             'page_obj': page_obj,
-             'rooms_list': room,
-            },
-            )
-
-
-# def add_room(request):
-#    if request.method == 'POST':
-#        form = RoomForm(request.POST, user)
-#        if form.is_valid():
-#            form.instance.creator = request.user.username
-#            form.save()
-#            return redirect('home')
-#    form = RoomForm()
-#    context = {
-#        'form': form
-#    }
-#    return render(request, '../templates/add_room.html', context)
+class RoomList(generic.ListView):
+    model = Rooms
+    queryset = Rooms.objects.order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 5
 
 
 class addRoom(View):
@@ -71,3 +40,17 @@ class addRoom(View):
         }
         return render(request, '../templates/add_room.html', context)
 
+
+class RoomView(View):
+    def get(self, request, slug, *args, **kwargs):
+        room_queryset = Rooms.objects.all()
+        room = get_object_or_404(room_queryset, slug=slug)
+        conversation = room.conversations.order_by('created_on')
+
+        return render(
+            request,
+            'room_view.html',
+            {
+             'conversation_list': conversation,
+            },
+            )
