ea6b67cfdbfd288b2d5ce36749f80f29f98b32e8
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-02-24 09:15:23 +0000

    clean up url paths

--- forum/urls.py
+++ forum/urls.py
@@ -2,13 +2,49 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.RoomList.as_view(), name='home'),
-    path('about', views.about.as_view(), name='about'),
-    path('your_room', views.YourRoomList.as_view(), name='your_home'),
-    path('<slug:slug>/', views.RoomView.as_view(), name='in_room'),
-    path('delete_room/<room_id>', views.delete_room, name='delete'),
-    path('delete_conversation/<conversation_id>', views.delete_conversation, name='delete_conversation'),
-    path('delete_comment/<comment_id>', views.delete_comment, name='delete_comment'),
-    path('edit_conversation/<conversation_id>', views.edit_conversation.as_view(), name='edit'),
-    path('edit_comment/<comment_id>', views.edit_comment.as_view(), name='edit_comment'),
+    path(
+        '',
+        views.RoomList.as_view(),
+        name='home'
+    ),
+    path(
+        'about',
+        views.about.as_view(),
+        name='about'
+    ),
+    path(
+        'your_room',
+        views.YourRoomList.as_view(),
+        name='your_home'
+    ),
+    path(
+        '<slug:slug>/',
+        views.RoomView.as_view(),
+        name='in_room'
+    ),
+    path(
+        'delete_room/<room_id>',
+        views.delete_room,
+        name='delete'
+    ),
+    path(
+        'delete_conversation/<conversation_id>',
+        views.delete_conversation,
+        name='delete_conversation'
+    ),
+    path(
+        'delete_comment/<comment_id>',
+        views.delete_comment,
+        name='delete_comment'
+    ),
+    path(
+        'edit_conversation/<conversation_id>',
+        views.edit_conversation.as_view(),
+        name='edit'
+    ),
+    path(
+        'edit_comment/<comment_id>',
+        views.edit_comment.as_view(),
+        name='edit_comment'
+    ),
 ]
